42c12eef4431b6d9c1cb6d672969a9be5d515f5c
mmateosalgado <salgadomateo001@gmai.com>
2025-11-06 11:57:52 -0300

    -deleted landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { ProfileComponent } from './auth/profile/profile.component';
-import { LandingComponent } from './landing/landing.component';
+import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-  { path: '', component: LandingComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '/' }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'profile',component:ProfileComponent,canActivate:[AuthGuard]},
+  { path: '**', redirectTo: '/login' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
